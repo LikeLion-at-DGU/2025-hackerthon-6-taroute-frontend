ddefcd59b8b714b1aef120104a9580f4ff6ff09b
김연비 <yeonbee@gim-yeonbiui-MacBookPro.local>
2025-08-11 14:32:13 +0900

    feature/#8: 홈화면 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './layouts/Layout.jsx'
-import HomePage from './pages/Home.jsx'
-import NotFoundPage from './pages/NotFound.jsx'
-import Taro from './pages/Taro.jsx'
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/taro" element={<Taro />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-=======
 import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
@@ -27,7 +8,6 @@ function App() {
     <>
       <Home/>
     </>
->>>>>>> 4e4f39e (feature:home 홈페이지)
   )
 }
 
